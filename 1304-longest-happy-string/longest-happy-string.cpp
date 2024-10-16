1845ced89dad7be68532190144c4ff6ffdedde5d
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2024-10-16 10:13:08 +0530

    Time: 3 ms (43.32%) | Memory: 7.3 MB (99.66%) - LeetSync

--- 1304-longest-happy-string/longest-happy-string.cpp
+++ 1304-longest-happy-string/longest-happy-string.cpp
@@ -1,38 +1,35 @@
 class Solution {
 public:
     string longestDiverseString(int a, int b, int c) {
-        priority_queue<pair<int, char >> pq ;
-
-        if( a > 0 ) pq.push( {a, 'a' } ) ;
-        if( b > 0 ) pq.push( {b, 'b' } ) ;
-        if( c > 0 ) pq.push( {c, 'c' } ) ;
-
+        int curra = 0, currb = 0 , currc = 0 ;
+        int totalIterations = a + b + c ;
         string ans = "" ;
 
-        while( !pq.empty() ) {
-            pair<int, char> p = pq.top() ;
-            pq.pop() ;
-            int count = p.first ;
-            char character = p.second ;
-
-            if( ans.length() >= 2 && ans[ans.length()-1 ] == p.second && ans[ans.length() -2 ] == p.second ) {
-                if( pq.empty() ) break ;
-
-                pair<int, char> temp = pq.top() ;
-                pq.pop() ;
-                ans += temp.second ;
-
-                if( temp.first -1 > 0 ) {
-                    pq.push( {temp.first - 1, temp.second} ) ;
-                }
-            }
+        for( int i = 0 ; i < totalIterations ; i++ ) {
             
-            else { 
-                count-- ;
-                ans = ans + character ;
+            if ( ( a >= b && a >= c && curra != 2 ) || ( a > 0 && ( currb == 2  || currc == 2 ) ) ){
+                ans += 'a' ;
+                a-- ;
+                curra++ ;
+                currb = 0 ;
+                currc = 0 ;
+            } 
+
+            else if( ( b >= a && b >= c && currb != 2 ) || ( b > 0 && ( currc == 2 || curra == 2 ) ) ) {
+                ans += 'b' ;
+                b-- ;
+                currb++ ;
+                curra = 0 ;
+                currc = 0 ;
             }
 
-            if( count > 0 ) pq.push( {count , character } ) ;
+            else if( ( c >= a && c >= b && currc != 2 ) || ( c > 0 && ( curra == 2 || currb == 2 ) ) ) {
+                ans += 'c' ;
+                c-- ;
+                currc++ ;
+                curra = 0 ;
+                currb = 0 ;
+            }
         }
 
         return ans ;
