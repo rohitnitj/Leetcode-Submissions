0707f8705799dcb067c3018cba6aa08e0cf5d73c
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2024-10-22 14:23:24 +0530

    Time: 21 ms (97.56%) | Memory: 156.2 MB (5.08%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     long long kthLargestLevelSum(TreeNode* root, int k) {
-        priority_queue<long long > pq ;
+        priority_queue<long long, vector<long long>, greater<long long> > pq ;
         queue<TreeNode*> bfs ;
-        bfs.push(root) ;
+        bfs.push(root );
 
         while( !bfs.empty() ) {
-            long long size = bfs.size() ;
+            int size = bfs.size() ;
             long long sum = 0 ;
 
             for( int i = 0 ; i < size ; i++ ) {
@@ -19,10 +19,10 @@ public:
             }
 
             pq.push(sum) ;
+            if ( pq.size() > k ) pq.pop() ;
         }
 
         if( pq.size() < k ) return -1 ;
-        for(int i = 0 ; i < k-1 ; i++ ) pq.pop() ;
         return pq.top() ;
     }
 };
