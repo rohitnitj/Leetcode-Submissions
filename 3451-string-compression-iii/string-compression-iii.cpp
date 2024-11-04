002b5c3b640b5dd5437bcafa989457264492ef8e
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2024-11-04 06:48:52 +0530

    Time: 53 ms (28.21%) | Memory: 29.9 MB (42.73%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.cpp
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     string compressedString(string word) {
-
         int n = word.size() ;
         string ans = "" ;
-        if( n == 1 ) return to_string(1) + word ;
+        int i = 0 ;
 
-        int cnt = 1 ;
-        for( int i = 0 ; i < n-1 ; i++) {
-            if( word[i+1] == word[i] ) cnt++ ;
-            else{
-                int quot = cnt / 9 ;
-                int rem = cnt % 9 ;
-                while( quot--) ans += to_string(9) + word[i] ;
-                if( rem > 0 ) ans += to_string(rem) + word[i] ;
-                cnt = 1 ;
+        while( i < n ) {
+            int j = 0 ; 
+            char ch = word[i] ;
+
+            while( i < n && j < 9 && word[i] == ch ) {
+                i++ ; 
+                j++ ;
             }
+
+            ans += to_string(j) + ch ;
         }
 
-        if( cnt > 1 ) {
-            int quot = cnt / 9 ;
-            int rem = cnt % 9 ;
-            while( quot--) ans += to_string(9) + word[n-1] ;
-            if( rem > 0 ) ans += to_string(rem) + word[n-1] ;
-        }
-
-        if( word[n-1] != word[n-2] && n > 1) ans += to_string(1) + word[n-1] ;
-
-        return ans ; 
-
+        return ans ;
     }
 };
