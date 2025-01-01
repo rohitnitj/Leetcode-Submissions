a973c23f03410c52c04313152cbbb4144e34b0bf
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-01-01 14:04:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (52.60%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,24 +1,18 @@
-class Solution {
-public:
-    int maxScore(string s) {
-        int n = s.size() ;
-        int one = 0 ;
-        int zero = 0 ;
+    class Solution {
+    public:
+        int maxScore(string s) {
+            int n = s.size() ;
+            int one = 0 ;
+            int zero = 0 ;
+            int maxi = INT_MIN ;
 
-        if( s[0] == '0' ) zero++ ;
+            for( int i = 0 ; i < n-1 ; i++ ) {
+                if( s[i] == '0' ) zero++ ;
+                else one++ ;
+                maxi = max(maxi, zero - one ) ;
+            }
 
-        for( int i = 1 ; i < n ; i++ ) {
-            if( s[i] == '1' ) one++ ;
+            if( s[n-1] == '1' ) one++ ;
+            return maxi + one ;
         }
-
-        int maxi = one + zero  ;
-        
-        for( int i = 1 ; i < n-1 ; i++ ) {
-            if( s[i] == '0' ) zero++ ;
-            else if( s[i] == '1') one-- ;
-            maxi = max(maxi, zero + one ) ;
-        }
-
-        return maxi ;
-    }
-};
+    };
