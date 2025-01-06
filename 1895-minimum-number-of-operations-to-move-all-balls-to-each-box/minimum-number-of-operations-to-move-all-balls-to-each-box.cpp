ea61ba6c8adae76b7e638d3a87d6bf63e960f6bd
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-01-06 22:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (40.56%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,4 +1,4 @@
-// TC : O(n^2) 
+// TC : O(n ) 
 // SC : O(n ) 
 
 class Solution {
@@ -7,16 +7,19 @@ public:
         int n = boxes.size() ;
         vector<int> ans(n ) ;
 
+        int left_balls = 0 , moves_left = 0 ;
+        int right_balls = 0, moves_right = 0 ;
+
         for( int i = 0 ; i < n ; i++ ) {
-            int cnt = 0 ;
+            
+            ans[i] += moves_left ;
+            left_balls += boxes[i] - '0' ;
+            moves_left += left_balls ;
 
-            for( int j = 0 ; j < n ; j++ ) {
-                if( boxes[j] == '1' ) {
-                    cnt += abs( i - j ) ;
-                }
-            }
-
-            ans[i] = cnt ;
+            int j = n - i - 1 ;
+            ans[j] += moves_right ;
+            right_balls += boxes[j] - '0' ;
+            moves_right += right_balls ;
         }
 
         return ans ; 
