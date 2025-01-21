3088f4d5f3bb14fab4ddc0158d03c95c174b6350
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-01-21 09:31:07 +0530

    Time: 0 ms (100.00%) | Memory: 66.6 MB (76.92%) - LeetSync

--- 2145-grid-game/grid-game.cpp
+++ 2145-grid-game/grid-game.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     long long gridGame(vector<vector<int>>& grid) {
         int n = grid[0].size() ;
-        long int lsum = 0, rsum = 0 ;
+        long long int lsum = 0, rsum = 0 ;
         for( int i = 0 ; i < n ; i++ ) {
             rsum += grid[0][i] ;
         }
 
-        long ans = LONG_MAX ;
+        long long ans = LONG_LONG_MAX ;
         for( int i = 0 ; i < n ; i++ ) {
             rsum -= grid[0][i] ;
             ans = min(ans, max(lsum, rsum ) ) ;
