154e735e0d85ae8beb8d222d7b5fd03a95441c2d
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-02-03 23:44:19 +0530

    Time: 45 ms (77.76%) | Memory: 70.8 MB (98.17%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,19 +1,33 @@
 class Solution {
+private:
+    void heapify(vector<int> &arr, int n, int i ) {
+        
+        int larget = i ;
+        int l = 2*i + 1 ;
+        int r = 2*i + 2 ;
+
+        if( l < n && arr[l] > arr[larget] ) larget = l ;
+        if( r < n && arr[r] > arr[larget] ) larget = r ;
+
+        if( larget != i ) {
+            swap( arr[i], arr[larget] ) ;
+            heapify(arr, n , larget ) ;
+        }
+    }
+
 public:
     vector<int> sortArray(vector<int>& nums) {
         int n = nums.size() ;
-        priority_queue<int> pq ;
 
-        for(int i = 0 ; i < n ; i++ ) {
-            pq.push(nums[i] ) ;
+        for( int i = n/2 - 1 ; i >= 0 ; i-- ) {
+            heapify(nums, n, i ) ;
         }
 
-        vector<int> ans(n) ;
-        for( int i = n-1 ; i >= 0 ; i-- ) {
-            ans[i] = pq.top() ;
-            pq.pop() ;
+        for( int i = n-1 ; i > 0 ; i-- ) {
+            swap(nums[0], nums[i] ) ;
+            heapify(nums, i , 0 ) ;
         }
 
-        return ans ;
+        return nums ;
     }
 };
