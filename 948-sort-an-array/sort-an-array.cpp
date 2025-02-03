0e9b8fa3413a848da0fcf676710c081ae979fa0e
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-02-03 22:59:46 +0530

    Time: 39 ms (80.00%) | Memory: 85.8 MB (50.07%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,31 +1,58 @@
 class Solution {
+private:    
+    void countSort(vector<int> &arr, int n, int p) {
+        vector<int> ans(n), cnt(10, 0);
+
+        for (int i = 0; i < n; i++) {
+            cnt[(arr[i] / p) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            cnt[i] += cnt[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            ans[cnt[(arr[i] / p) % 10] - 1] = arr[i];
+            cnt[(arr[i] / p) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = ans[i];
+        }
+    }
+
+    void radixSort(vector<int>& arr) {
+        if (arr.empty()) return;
+        int maxi = *max_element(arr.begin(), arr.end());
+
+        for (int p = 1; maxi / p > 0; p *= 10) {
+            countSort(arr, arr.size(), p);
+        }
+    }
+
 public:
     vector<int> sortArray(vector<int>& nums) {
-        int n = nums.size() ;   
-
-        int maxi = -1e9, mini = 1e9 ;
-        for( int i = 0 ; i < n ; i++ ) {
-            maxi = max(maxi, nums[i] ) ;
-            mini = min(mini, nums[i] ) ;
+        vector<int> positive, negative;
+        
+        for (int num : nums) {
+            if (num < 0) {
+                negative.push_back(-num);
+            } else {
+                positive.push_back(num);
+            }
         }
 
-        int m = maxi - mini ;
+        radixSort(positive);
+        radixSort(negative);
 
-        vector<int> cnt(m+1, 0 ) ;
-        for( int i = 0 ; i < n ; i++ ) {
-            cnt[nums[i]-mini]++ ;
+        vector<int> sortedArray;
+        
+        for (int i = negative.size() - 1; i >= 0; i--) {
+            sortedArray.push_back(-negative[i]);
         }
 
-        for(int i = 1 ; i <= m ; i++ ) {
-            cnt[i] += cnt[i-1] ;
-        }
+        sortedArray.insert(sortedArray.end(), positive.begin(), positive.end());
 
-        vector<int> ans(n ) ;
-        for( int i = n-1 ; i >= 0 ; i-- ) {
-            cnt[nums[i]-mini]-- ;
-            ans[cnt[nums[i]-mini]] = nums[i] ;
-        }
-
-        return ans ;
+        return sortedArray;
     }
 };
