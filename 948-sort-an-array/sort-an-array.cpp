c779dcd0cc7bfe52613acce7b5e376c9c48224e3
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-02-03 22:24:27 +0530

    Time: 2 ms (99.64%) | Memory: 72.8 MB (68.01%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end() ) ;
-        return nums ; 
+        int n = nums.size() ;   
+
+        int maxi = -1e9, mini = 1e9 ;
+        for( int i = 0 ; i < n ; i++ ) {
+            maxi = max(maxi, nums[i] ) ;
+            mini = min(mini, nums[i] ) ;
+        }
+
+        int m = maxi - mini ;
+
+        vector<int> cnt(m+1, 0 ) ;
+        for( int i = 0 ; i < n ; i++ ) {
+            cnt[nums[i]-mini]++ ;
+        }
+
+        for(int i = 1 ; i <= m ; i++ ) {
+            cnt[i] += cnt[i-1] ;
+        }
+
+        vector<int> ans(n ) ;
+        for( int i = n-1 ; i >= 0 ; i-- ) {
+            cnt[nums[i]-mini]-- ;
+            ans[cnt[nums[i]-mini]] = nums[i] ;
+        }
+
+        return ans ;
     }
 };
