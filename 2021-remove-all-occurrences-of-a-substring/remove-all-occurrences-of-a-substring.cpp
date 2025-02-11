9beda683ee01422ade2877e264dd882156509c3e
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-02-11 19:42:00 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (12.11%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,14 +1,38 @@
 class Solution {
+private:
+    vector<int> kmp(string s ) {
+        vector<int> lps(s.size(), 0 ) ;
+        
+        for( int i = 1 ; i < lps.size() ; i++ ) {
+            int prev_idx = lps[i-1] ;
+
+            while(prev_idx > 0 && s[i] != s[prev_idx] ) {
+                prev_idx = lps[prev_idx-1] ;
+            }
+
+            lps[i] = prev_idx + ( s[i] == s[prev_idx] ? 1 : 0 ) ;
+        }
+
+        return lps ;
+    }
+
 public:
     string removeOccurrences(string s, string part) {
-        
-        int n = part.size() ;
-        string ans = "" ;
+        vector<int> lps = kmp(part ) ;
+        string ans ;
+        vector<int> pre_matches ;
 
-        for ( char ch : s ) {
-            ans += ch ;
-            if( ans.size() >= n && ans.substr(ans.size() - n ) == part ) {
-                ans.erase(ans.size() - n ) ;
+        for( char c : s ) {
+            ans.push_back(c ) ;
+            int j = pre_matches.empty() ? 0 : pre_matches.back() ;
+
+            while( j > 0 && part[j] != c ) j = lps[j-1] ;
+            if( part[j] == c ) j++ ;
+            pre_matches.push_back(j) ;
+
+            if( j == part.size() ) {
+                ans.resize(ans.size() - part.size() ) ;
+                pre_matches.resize(pre_matches.size() - part.size() ) ;
             }
         }
 
