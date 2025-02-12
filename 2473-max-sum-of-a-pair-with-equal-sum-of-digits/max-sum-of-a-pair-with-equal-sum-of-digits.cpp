98095399ebad00084b4837e1b2cea6eb3e1e5bc7
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-02-12 19:18:00 +0530

    Time: 2 ms (99.25%) | Memory: 63.8 MB (87.26%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,42 +1,31 @@
 class Solution {
 private:
-    int digitSum(int n ){
-        int sum = 0 ;
-        while( n ) {
-            sum += n % 10 ;
-            n /= 10 ;
+    int digitSum(int n) {
+        int sum = 0;
+        while (n) {
+            sum += n % 10;
+            n /= 10;
         }
-        return sum ;
+        return sum;
     }
 
 public:
     int maximumSum(vector<int>& nums) {
-        int n = nums.size() ;
+        int n = nums.size();
 
-        vector< priority_queue<int, vector<int>, greater<int> >> mpp(82 ) ;
+        vector<int> mpp(82, 0);
+        int ans = -1;
 
-        for( auto num : nums ) {
-            int dsum = digitSum(num ) ;
-            mpp[dsum].push(num ) ;
+        for (auto num : nums) {
+            int dsum = digitSum(num);
 
-            if(mpp[dsum].size() > 2 ) {
-                mpp[dsum].pop();
+            if (mpp[dsum] != 0) {
+                ans = max(ans, mpp[dsum] + num);
             }
+
+            mpp[dsum] = max(mpp[dsum], num);
         }
 
-        int ans = -1 ;
-        
-        for( auto &pq : mpp ) {
-            if( pq.size() >= 2 ) {
-                
-                int first = pq.top() ;
-                pq.pop() ;
-                int second = pq.top() ;
-
-                ans = max( ans, first + second ) ;
-            }
-        }
-
-        return ans ;
+        return ans;
     }
 };
