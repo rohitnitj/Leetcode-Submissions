584f854ddf0c8ba04a7dd6589b4a5b192def05fb
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-02-17 16:30:55 +0530

    Time: 35 ms (41.26%) | Memory: 18 MB (25.71%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.cpp
@@ -1,39 +1,25 @@
 class Solution {
-private:
-    void generatePermutaions(string &s, int idx, int n, unordered_set<string> &st ) {
-        if( idx == n ) {
-            st.insert(s) ;
-            return ;
+private:    
+    void func(string &tiles, string temp, vector<bool> &used, unordered_set<string> &st ) {
+        st.insert(temp) ;
+
+        for( int i = 0 ; i < tiles.length() ; i++ ) {
+            if( !used[i] ) {
+                used[i] = true ;
+                func(tiles, temp + tiles[i], used, st ) ;
+                used[i] = false ;
+            }
         }
-
-        for( int i = idx ; i < n ; i++ ) {
-            swap(s[i], s[idx] ) ;
-            generatePermutaions(s, idx+1 , n, st ) ;
-            swap(s[i], s[idx] ) ;
-        }
-    }
-
-    void func(string &tiles, unordered_set<string> &st, string &temp, int idx, int n ) {
-        if( idx == n ) {
-            generatePermutaions(temp, 0, temp.size(), st ) ;
-            return ;
-        }
-
-        temp.push_back(tiles[idx] ) ;
-        func(tiles, st, temp, idx+1, n ) ;
-
-        temp.pop_back() ;
-        func(tiles, st, temp, idx+1, n ) ;
-
     }
 
 public:
     int numTilePossibilities(string tiles) {
-        unordered_set<string> st ;
-        string temp ;
         int n = tiles.size() ;
-        func(tiles, st, temp, 0, n ) ;
+        unordered_set<string> st ;
+        vector<bool> used(n, false ) ;
 
+        func(tiles, "", used, st ) ;
+        
         return st.size()-1 ;
     }
 };
