2b238c5a9d3cb047e23468ab5f661285b8577344
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-03-04 10:15:27 +0530

    Time: 1425 ms (5.40%) | Memory: 319.3 MB (9.62%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,13 +1,16 @@
 class Solution {
 private:
-    bool func(int idx, int ans, int n) {
-        if( ans == n ) return true ;
-        if( pow(3, idx ) > n ) return false ;
-        return func(idx+1, ans + pow(3, idx), n ) || func(idx+1, ans, n ) ;
+    bool func(int idx, int sum, int n, unordered_map<string, bool>& dp) {
+        string key = to_string(idx) + "," + to_string(sum);
+        if (dp.find(key) != dp.end()) return dp[key];
+        if (sum == n) return dp[key] = true;
+        if (pow(3, idx) > n) return dp[key] = false;
+        return dp[key] = func(idx + 1, sum + pow(3, idx), n, dp) || func(idx + 1, sum, n, dp);
     }
 
 public:
     bool checkPowersOfThree(int n) {
-        return func(0, 0, n) ;
+        unordered_map<string, bool> dp;
+        return func(0, 0, n, dp);
     }
 };
