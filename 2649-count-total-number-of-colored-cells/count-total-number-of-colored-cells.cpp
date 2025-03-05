4a7619f288bb692febcf53310c9473dfc44d9443
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-03-05 15:33:12 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (31.17%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,16 +1,8 @@
 typedef long long ll ;
 
 class Solution {
-private:
-    ll f(int n, vector<ll> &dp ) {
-        if( n == 1) return ll(1) ;
-        if( dp[n] != -1 ) return dp[n] ;
-        return dp[n] = ll((ll)4*(n-1) + f(n-1, dp )) ;
-    }
-
 public:
     long long coloredCells(int n ) {
-        vector<ll> dp(n+1, -1) ;
-        return f(n, dp ) ;
+        return ll(1 + 2 * ll(n) * ll(n-1 )) ;
     }
 };
