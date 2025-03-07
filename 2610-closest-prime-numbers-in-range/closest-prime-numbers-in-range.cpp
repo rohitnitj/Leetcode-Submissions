073b1ed7a993721dcfd57572b8bf6da255d88e41
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-03-07 21:15:10 +0530

    Time: 153 ms (69.81%) | Memory: 168.1 MB (9.44%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,22 +1,27 @@
 class Solution {
 private:
-    bool isPrime(int n ) {
-        if( n == 1 ) return false ;
-        bool f = true ;
-        for( int i = 2; i*i <= n ; i++ ) {
-            if( n % i == 0 ) {
-                f = false ;
-                break ;
+    vector<int> seive(int n ) {
+        vector<int> seive(n+1, 1 ) ;
+        seive[0] = seive[1] = 0 ;
+
+        for( int i = 2 ; i*i <= n ; i++ ) {
+            if( seive[i] == 1 ) {
+
+                for( int j = i*i ; j <= n ; j += i ) {
+                    seive[j] = 0 ;
+                }
             }
         }
-        return f ;
+
+        return seive ;
     }
 
 public:
     vector<int> closestPrimes(int left, int right) {
+        vector<int> seive_arr = seive(right ) ;
         vector<int> arr ;
         for (int i = left ; i <= right; i++ ) {
-            if( isPrime(i ) ) arr.push_back(i) ;
+            if( seive_arr[i] ) arr.push_back(i) ;
         }
 
         if( arr.size() <= 1 ) return {-1, -1} ;
@@ -26,7 +31,6 @@ public:
         vector<int> ans ;
         for(int i = 0; i < arr.size() - 1 ; i++ ) {
             if( abs(arr[i+1] - arr[i] ) < mini ) {
-                // cout << arr[i] << " " << arr[i+1] << endl ;
                 ans = {arr[i], arr[i+1] } ;
                 mini = abs( arr[i+1] -arr[i] ) ;
             }
