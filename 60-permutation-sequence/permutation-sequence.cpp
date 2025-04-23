181f7428f02a611deca19917ea759b34bac3ed7a
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-04-23 18:31:40 +0530

    Time: 83 ms (8.10%) | Memory: 8.5 MB (77.12%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -1,26 +1,36 @@
 class Solution {
 private:
+    void nextPermutation(string& nums) {
+        int n = nums.size() ;
+        int ind = -1 ;
+        for(int i = n-2 ; i >= 0 ; i-- ) {
+            if( nums[i] < nums[i+1] ) {
+                ind = i ;
+                break ;
+            }
+        }
+
+        if( ind == -1 ){
+            reverse(nums.begin(), nums.end() ) ;
+            return ;
+        }
+
+        for( int i = n-1 ; i > ind ; i-- ) {
+            if( nums[i] > nums[ind] ) {
+                swap(nums[i], nums[ind] ) ;
+                break ;
+            }
+        }
+
+        reverse(nums.begin() + ind + 1, nums.end() ) ;
+    }    
+
 public:
     string getPermutation(int n, int k) {
-        int fact = 1 ;
-        vector<int> numbers;
-        for( int i = 1; i < n ; i++ ) {
-            fact *= i ;
-            numbers.push_back(i) ;
-        }
-        numbers.push_back(n) ;
+        string s = "";
+        for( int i = 1; i <= n ; i++ ) s += to_string(i) ;
 
-        string ans = "" ;
-        k-- ;
-
-        while( true ) {
-            ans += to_string(numbers[k/fact]) ;
-            numbers.erase(numbers.begin() + k / fact ) ;
-            if( numbers.size() == 0 ) break ;
-            k = k % fact ;
-            fact = fact / numbers.size() ;
-        }
-
-        return ans ;
+        for(int i = 1; i < k; i++) nextPermutation(s);
+        return s;
     }
 };
