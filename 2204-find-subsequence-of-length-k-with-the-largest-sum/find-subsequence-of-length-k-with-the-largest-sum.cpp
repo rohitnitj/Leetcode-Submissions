fad4f518df0d9091c4231831489ed2484ac2d29c
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-06-28 16:13:07 +0530

    Time: 1 ms (57.19%) | Memory: 13.9 MB (43.60%) - LeetSync

---
+++ 2204-find-subsequence-of-length-k-with-the-largest-sum/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        int n = nums.size() ;
+        
+        vector<pair<int,int>> arr ;
+        for( int i = 0 ; i < n; i++ ) arr.push_back({i, nums[i]} ) ;
+
+        sort( arr.begin(), arr.end(), 
+            [&](auto x1, auto x2 ) {return x1.second > x2.second; } ) ;
+
+        sort(arr.begin(), arr.begin() + k ) ;
+
+        vector<int> ans ;
+        for( int i = 0 ; i < k ; i++ ) ans.push_back(arr[i].second) ;
+
+        return ans ;
+    }
+};
