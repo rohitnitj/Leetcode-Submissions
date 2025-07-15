a63b5cf023f0acbbb88cd8ddeabc0d33fb81d943
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-07-15 14:15:50 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (67.48%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -1,37 +1,22 @@
 class Solution {
 public:
-    bool isDigit(char ch ) {
-        if( ch >= '0' && ch <= '9' ) return true ;
-        return false ;
-    }
-
-    bool vowel(string word ) {
-        for( char ch : word ) {
-            if( ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
-                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ) return true ;
-        }
-        return false ;
-    }
-
-    bool consonant(string word ) {
-        for( char ch : word ) {
-            if( !(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
-                ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') && !isDigit(ch)) return true ;
-        }
-        return false ;
-    }
-
-    bool isAlNum(string word ) {
-        for( char ch : word ) {
-            if( (ch >= '0' && ch <= '9') || ( ch >= 'a' && ch <= 'z' ) || ( ch >= 'A' && ch <= 'Z' ) ) continue ;
-            return false ; 
-        }
-        return true ;
-    }
-
     bool isValid(string word) {
-        int n = word.size() ;
-        if( n >= 3 && isAlNum(word ) && vowel(word) && consonant(word ) ) return true ;
-        return false ;
+        if (word.length() < 3)
+            return false;
+
+        bool hasVowel = false, hasConsonant = false;
+        for (char c : word) {
+            if (!isalnum(c))
+                return false;
+
+            if (isalpha(c)) {
+                char lower = tolower(c);
+                if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u')
+                    hasVowel = true;
+                else
+                    hasConsonant = true;
+            }
+        }
+        return hasVowel && hasConsonant;
     }
 };
