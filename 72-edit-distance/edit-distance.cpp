178529236959fcd874bf945a9aafc41a26d3e65f
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-07-20 21:01:25 +0530

    Time: 5 ms (71.50%) | Memory: 13 MB (74.27%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,27 +1,24 @@
+// tabulation
 class Solution {
 public:
     int minDistance(string s1, string s2) {
         int n = s1.size() ;
         int m = s2.size() ;
+        vector<vector<int>> dp(n+1, vector<int> (m+1, 0 ) ) ;
 
-        vector<int> prev(m+1 , 0 ) ;
-        vector<int> curr(m+1 , 0 ) ;
-
-        for( int i = 0 ; i <= m ;i++ ) prev[i] = i ;
+        for( int i = 0 ; i <= n ;i++ ) dp[i][0] = i ;
+        for( int j = 0 ; j <= m ;j++ ) dp[0][j] = j ;
 
         for( int i = 1 ; i <= n ;i++ ) {
-            curr[0] = i ;
-
             for( int j = 1 ; j <= m ; j++ ) {
 
                 if( s1[i-1] == s2[j-1] ) 
-                    curr[j] = prev[j-1] ;
+                    dp[i][j] = dp[i-1][j-1] ;
                 else
-                    curr[j] = 1 + min( prev[j] , min( curr[j-1], prev[j-1] ) ) ;
+                    dp[i][j] = 1 + min( dp[i-1][j] , min( dp[i][j-1], dp[i-1][j-1] ) ) ;
             }
-            prev = curr ;
         }
 
-        return prev[m] ;
+        return dp[n][m] ;
     }   
 };
