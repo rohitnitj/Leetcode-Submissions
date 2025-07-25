65e85546cb4cff7fa16650dab83a1e1713fe7bd2
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-07-25 09:24:00 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (86.18%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -12,14 +12,8 @@
 class Solution {
 private:
     bool helper(TreeNode* p, TreeNode* q ) {
-        if( p == NULL || q == NULL ) {
-            return p == q ;
-        }
-
-        if( p -> val != q -> val ) {
-            return false ;
-        }
-
+        if( p == NULL || q == NULL ) return p == q ;
+        if( p -> val != q -> val ) return false ;
         return helper( p -> left, q -> right ) && helper( p -> right, q -> left ) ;
     }
 
