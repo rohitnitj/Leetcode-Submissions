30d8bd24948c4d66ad2cab4c14991c62155f7cae
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-08-14 16:30:12 +0530

    Time: 48 ms (57.99%) | Memory: 57.5 MB (35.95%) - LeetSync

--- 2789-counter-ii/counter-ii.js
+++ 2789-counter-ii/counter-ii.js
@@ -6,15 +6,15 @@ var createCounter = function(init) {
     let count = init;
 
     return {
-        increment: function() {
+        increment() {
             count++;
             return count;
         },
-        decrement: function() {
+        decrement() {
             count--;
             return count;
         },
-        reset: function() {
+        reset() {
             count = init;
             return count;
         }
