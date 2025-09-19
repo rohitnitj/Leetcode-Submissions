82ba95cfaadb9c8fe1d141d4ba82ae3056419567
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-09-20 00:23:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size() ;
-        int prev = nums[0] ;
-        int prev2 = 0 ;
 
-        for( int i = 1 ;i < n ;i++ ) {
-            int take = nums[i] + prev2 ;
-            int nontake = 0 + prev ;
+        // base case
+        if( n == 1 ) return nums[0];
+        if( n == 2 ) return max(nums[0], nums[1] ) ;
 
-            int curri = max( take, nontake ) ;
-            prev2 = prev ;
-            prev = curri ;
+        // dp[i] = max money till index i 
+        vector<int> dp(n) ;
+        dp[0] = nums[0] ;
+        dp[1] = max(nums[0], nums[1] ) ;
+
+        // transition : take or not Take
+        for( int i = 2 ; i < n ; i++ ) {
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1] ) ;
         }
 
-        return prev ;
+        return dp[n-1] ;
     }
 };
