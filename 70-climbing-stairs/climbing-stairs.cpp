47a626bcee322c6b891a78501a965a95bd419820
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-09-20 00:12:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (36.73%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        // base case
         if( n <= 2 ) return n ; 
+        
+        // dp[i] = number of distinct ways to climb to dp[i] ;
         vector<int> dp(n+1, 0 ) ;
         dp[1] = 1, dp[2] = 2 ;
-        for( int i = 3 ; i <= n ; i++ ) dp[i] = dp[i-1] + dp[i-2] ;
+
+        // state transistion
+        for( int i = 3 ; i <= n ; i++ ) {
+            dp[i] = dp[i-1] + dp[i-2]  ;
+        }
+
+        // number of ways to climb at top
         return dp[n] ;
     }
 };
