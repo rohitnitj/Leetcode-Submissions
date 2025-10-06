8a4875e1a46de53d629053e7690515e0e33336e3
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-10-06 14:48:48 +0530

    Time: 1 ms (92.89%) | Memory: 13.8 MB (57.21%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,31 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    struct T {
-        int t, x, y;
-        T(int a, int b, int c) : t(a), x(b), y(c) {}
-        bool operator<(const T &d) const { return t > d.t; } // Min-heap
-    };
-
     int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size(), ans = 0;
-        priority_queue<T> pq;
-        pq.push(T(grid[0][0], 0, 0));
-        vector<vector<int>> seen(n, vector<int>(n, 0));
-        seen[0][0] = 1;
-        static int dir[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-
-        while (true) {
-            auto p = pq.top(); pq.pop();
-            ans = max(ans, p.t);
-            if (p.x == n - 1 && p.y == n - 1) return ans;
-
-            for (auto &d : dir) {
-                int i = p.x + d[0], j = p.y + d[1];
-                if (i >= 0 && i < n && j >= 0 && j < n && !seen[i][j]) {
-                    seen[i][j] = 1;
-                    pq.push(T(grid[i][j], i, j));
+        int n = grid.size();
+        vector<vector<int>> vis(n, vector<int>(n, 0));
+        
+        // min-heap: {elevation, x, y}
+        priority_queue<array<int,3>, vector<array<int,3>>, greater<>> pq;
+        pq.push({grid[0][0], 0, 0});
+        vis[0][0] = 1;
+        
+        int maxTime = 0;
+        int dx[4] = {1, -1, 0, 0};
+        int dy[4] = {0, 0, 1, -1};
+        
+        while (!pq.empty()) {
+            auto [elev, x, y] = pq.top();
+            pq.pop();
+            
+            maxTime = max(maxTime, elev);
+            
+            if (x == n - 1 && y == n - 1)
+                return maxTime;
+            
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+                
+                if (nx >= 0 && ny >= 0 && nx < n && ny < n && !vis[nx][ny]) {
+                    vis[nx][ny] = 1;
+                    pq.push({grid[nx][ny], nx, ny});
                 }
             }
         }
+        return -1; // should never reach here
     }
 };
