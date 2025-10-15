6ab2a42d07224d6076d4eee82799f122f3f7a912
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2025-10-15 15:47:41 +0530

    Time: 246 ms (22.93%) | Memory: 173.5 MB (98.12%) - LeetSync

--- 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/adjacent-increasing-subarrays-detection-ii.cpp
@@ -3,19 +3,18 @@ public:
     int maxIncreasingSubarrays(vector<int>& nums) {
         int n = nums.size() ;
 
-        vector<int> arr(n, 1 ) ;
-        for( int i = 1 ; i < n; i++ ) {
-            if( nums[i] > nums[i-1] ) arr[i] += arr[i-1] ;
-        }
-
-        int maxi = 0, ans = 0 ;
+        int cnt = 1, precnt = 0, ans = 0 ;
         for( int i = 1; i < n; i++ ) {
-            maxi = max(maxi, arr[i] ) ;
-            int idx = i - arr[i] ;
-            if( idx >= 0 ) ans = max(ans, min(arr[i], arr[idx] )) ;
+            if( nums[i] > nums[i-1] ) cnt++ ;
+            else {
+                precnt = cnt ;
+                cnt = 1 ;
+            }
+
+            ans = max(ans, min(cnt, precnt ) ) ;
+            ans = max(ans, cnt / 2 ) ;
         }
 
-        ans = max(ans, maxi / 2 ) ;
         return ans ; 
     }
 };
