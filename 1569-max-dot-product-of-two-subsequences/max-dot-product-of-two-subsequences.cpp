b2da45b0e5d15a3a98218e87f1f742f57e8afc9e
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2026-01-08 13:41:23 +0530

    Time: 10 ms (89.20%) | Memory: 17.7 MB (54.80%) - LeetSync

---
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        int m = nums2.size();
+
+        const int NEG = -1e9;
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, NEG));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int take = nums1[i - 1] * nums2[j - 1];
+                take += max(0, dp[i - 1][j - 1]);
+
+                dp[i][j] = max({
+                    take,
+                    dp[i - 1][j],
+                    dp[i][j - 1]
+                });
+            }
+        }
+
+        return dp[n][m];
+    }
+};
