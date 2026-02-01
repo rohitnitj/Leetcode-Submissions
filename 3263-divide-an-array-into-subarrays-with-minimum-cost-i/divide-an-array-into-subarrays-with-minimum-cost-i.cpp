7321a9462d09818ce0938246161927f5de0c440c
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2026-02-01 14:28:54 +0530

    Time: 2 ms (9.05%) | Memory: 31 MB (5.03%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     int minimumCost(vector<int>& nums) {
+        int n = nums.size() ;
+
+        priority_queue<int, vector<int>, greater<int> > pq ;
+        for( int i = 1; i < n; i++ ){
+            pq.push(nums[i] ) ;
+        }
+
         int ans = nums[0] ;
-        sort(nums.begin()+1, nums.end() ) ;
-        ans += nums[1] + nums[2] ;
-        return ans; 
+        for( int i = 0 ; i < 2 ; i++ ) {
+            if( pq.size() ) {
+                ans += pq.top() ;
+                pq.pop() ;
+            }
+        }
+
+        return ans ; 
     }
 };
