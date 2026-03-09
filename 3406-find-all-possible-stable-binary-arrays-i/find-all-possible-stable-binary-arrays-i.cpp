8f0fa3a85a35e40a7e66955ede45ec8a7f4254e8
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2026-03-09 11:25:47 +0530

    Time: 14 ms (89.43%) | Memory: 9 MB (64.23%) - LeetSync

---
+++ 3406-find-all-possible-stable-binary-arrays-i/find-all-possible-stable-binary-arrays-i.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+    const int MOD = 1e9 + 7;
+
+public:
+    int numberOfStableArrays(int zero, int one, int limit) {
+
+        static long long dp[201][201][2];
+        memset(dp, 0, sizeof(dp));
+
+        for(int i = 1; i <= zero; i++)
+            if(i <= limit) dp[i][0][0] = 1;
+
+        for(int j = 1; j <= one; j++)
+            if(j <= limit) dp[0][j][1] = 1;
+
+        for(int i = 1; i <= zero; i++) {
+            for(int j = 1; j <= one; j++) {
+
+                // end with 0
+                dp[i][j][0] = (dp[i-1][j][0] + dp[i-1][j][1]) % MOD;
+
+                if(i - limit - 1 >= 0)
+                    dp[i][j][0] = (dp[i][j][0] - dp[i-limit-1][j][1] + MOD) % MOD;
+
+                // end with 1
+                dp[i][j][1] = (dp[i][j-1][0] + dp[i][j-1][1]) % MOD;
+
+                if(j - limit - 1 >= 0)
+                    dp[i][j][1] = (dp[i][j][1] - dp[i][j-limit-1][0] + MOD) % MOD;
+            }
+        }
+
+        return (dp[zero][one][0] + dp[zero][one][1]) % MOD;
+    }
+};
