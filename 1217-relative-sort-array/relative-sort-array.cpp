41a4200e78f900c937e94aad45cc03e2dfaa98e8
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2026-06-15 01:33:58 +0530

    Time: 2 ms (23.68%) | Memory: 11.3 MB (6.77%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -2,30 +2,27 @@ class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
         int n1 = arr1.size() ; 
-        int n2 = arr2.size() ;
+        int n2 = arr2.size() ; 
 
-        map<int, int> freq ; 
-        for( int a : arr1 ) freq[a]++ ; 
+        map<int, int> cnt ; 
+        for( int a : arr1 ) cnt[a]++ ; 
 
         vector<int> ans ; 
-        
         for( int a : arr2 ) {
-            while( freq[a]){
-                ans.push_back( a ) ; 
-                freq[a]-- ; 
+            while( cnt[a] ) {
+                ans.push_back( a ) ;
+                cnt[a]-- ; 
             }
-        }
-
-        int idx = ans.size() ; 
+        }   
 
         set<int> st ; 
         for( int a : arr2 ) st.insert(a) ; 
 
-        for( int a : arr1 ) {
-            if( st.find(a) == st.end() ) ans.push_back(a) ; 
-        }
+        sort( arr1.begin(), arr1.end() ) ; 
 
-        sort( ans.begin() + idx, ans.end() ) ; 
+        for( int a : arr1 ) {
+            if( st.find( a ) == st.end() ) ans.push_back(a) ; 
+        }
 
         return ans ; 
     }
