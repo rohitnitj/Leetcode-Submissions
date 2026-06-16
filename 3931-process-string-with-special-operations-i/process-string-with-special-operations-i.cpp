2702a1930e844dff000f08fdfd56e3a57b6a497e
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2026-06-16 13:58:05 +0530

    Time: 7 ms (35.61%) | Memory: 39.8 MB (69.70%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans = "" ; 
+
+        for( char ch : s ) {
+            if( ch == '*' ){
+                if(ans.size()) ans.pop_back() ;
+            }
+            else if( ch == '%' ) reverse( ans.begin(), ans.end() ) ;
+            else if( ch == '#' ) ans += ans ; 
+            else ans += ch ; 
+        }
+
+        return ans ; 
+    }
+};
