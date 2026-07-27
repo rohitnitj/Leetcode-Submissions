56148368c48b3094044df9bf2fd0410e9d12b9a6
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2026-07-27 23:01:18 +0530

    Time: 4 ms (46.75%) | Memory: 27.6 MB (34.75%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,40 +11,29 @@
  */
 class Solution {
 public:
+    TreeNode* build( vector<int> &inorder, int inStart, int inEnd, vector<int> &postorder, int postStart, int postEnd, map<int, int> &inMap ) {
+        if( inStart > inEnd || postStart > postEnd ) return NULL ; 
 
-    TreeNode* formTree(vector<int> &inorder, int inStart , int inEnd,
-                       vector<int> &postorder, int postStart, int postEnd, unordered_map<int,int> &inMap ) {
-            
-        if ( postStart > postEnd || inStart > inEnd ) 
-            return NULL ;
+        TreeNode *root = new TreeNode( postorder[postEnd] ) ; 
         
-        TreeNode* root = new TreeNode(postorder[postEnd] ) ;
+        int inroot = inMap[root -> val ] ;
+        int numsleft = inroot - inStart ; 
 
-        int inRoot = inMap[postorder[postEnd]] ;
-        int numsLeft = inRoot - inStart ;
+        root -> left = build( inorder, inStart, inroot - 1, postorder, postStart, postStart + numsleft - 1, inMap ) ; 
+        root -> right = build( inorder, inroot + 1, inEnd, postorder, postStart + numsleft, postEnd - 1, inMap ) ; 
 
-        root -> left = formTree(inorder, inStart , inRoot -1 , 
-                                postorder, postStart, postStart + numsLeft -1 , inMap ) ;
-        root -> right = formTree(inorder, inRoot + 1 , inEnd ,
-                                postorder, postStart + numsLeft, postEnd -1 , inMap ) ;
-
-        return root ;
+        return root ; 
     }
 
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        int n = inorder.size() ;
 
-        if ( inorder.size() != postorder.size() ) 
-            return NULL ;
-
-        unordered_map<int,int> inMap ;
-
-        for( int i = 0 ; i < inorder.size() ; i++ ) {
-            inMap[inorder[i]] = i ;
+        map<int, int> inMap ; 
+        for( int i = 0 ; i < n; i++ ) {
+            inMap[inorder[i]] = i ; 
         }
 
-        TreeNode* root = formTree(inorder, 0, inorder.size()-1, 
-                                  postorder, 0, postorder.size() -1, inMap ) ;
-        
-        return root  ;
+        TreeNode *root = build( inorder, 0, n-1, postorder, 0, n-1, inMap ) ; 
+        return root ; 
     }
 };
