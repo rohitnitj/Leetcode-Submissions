cc3d794cd3d14e8b7c70d61c9bd3cde5878b2155
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2026-08-01 12:11:36 +0530

    Time: 4 ms (42.37%) | Memory: 18.5 MB (66.19%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -1,38 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
+    struct compare {
+        bool operator()(const ListNode* l, const ListNode* r) {
+            return l->val > r->val;
+        }
+    };
+
 public:
-    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        if( l1 == NULL ) return l2 ;
-        if( l2 == NULL ) return l1 ;
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
         
-        if( l1 -> val <= l2 -> val  ) {
-            l1 -> next = mergeTwoLists( l1 -> next, l2 ) ;
-            return l1 ;
+        for (ListNode* l : lists) {
+            if (l) pq.push(l);
         }
-        else {
-            l2 -> next = mergeTwoLists( l1, l2 -> next ) ;
-            return l2;
-        }
-    }
-
-    ListNode *mergeKLists(vector<ListNode *> &lists) {
-        if( lists.empty()) return NULL ;
-
-        while( lists.size() > 1 ) {
-            lists.push_back(mergeTwoLists(lists[0], lists[1] ) ) ;
-            lists.erase(lists.begin()) ;
-            lists.erase(lists.begin()) ;
         
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        
+        while (!pq.empty()) {
+            tail->next = pq.top();
+            pq.pop();
+            tail = tail->next;
+            
+            if (tail->next) {
+                pq.push(tail->next);
+            }
         }
-        return lists.front();
+        
+        return dummy.next;
     }
 };
