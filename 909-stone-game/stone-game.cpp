2db1561604e239a4c4f17288cc991b22163b9255
Rohit Kumar <148082988+rohitnitj@users.noreply.github.com>
2026-08-02 10:27:07 +0530

    Time: 9 ms (29.89%) | Memory: 19.6 MB (20.08%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -3,19 +3,16 @@ public:
     bool stoneGame(vector<int>& piles) {
         int n = piles.size() ; 
 
-        vector<vector<int>> dp(n+2, vector<int>(n+2 ) ) ; 
+        vector<vector<int>> dp(n, vector<int>(n ) ) ;
+        for( int i = 0 ; i < n; i++ ) dp[i][i] = piles[i] ; 
 
-        for( int size = 1; size <= n ; size++ ) {
-            for( int i = 0; i < n ; i++ ) {
-                int j = i + size - 1; 
-                if( j >= n ) break ; 
-
-                int parity = ( (j + i + n) & 1 ) ; 
-                if( parity ) dp[i+1][j+1] = max( piles[i] + dp[i+2][j+1], piles[j] + dp[i+1][j] ) ; 
-                else dp[i+1][j+1] = min( -piles[i] + dp[i+2][j+1], -piles[j] + dp[i+1][j] ) ; 
+        for( int len = 2 ; len <= n ; len++ ) {
+            for( int i = 0 ; i <= n - len ; i++ ) {
+                int j = i + len - 1 ;
+                dp[i][j] = max( piles[i] - dp[i+1][j], piles[j] - dp[i][j-1] ) ; 
             }
         }
 
-        return dp[1][n] > 0 ; 
+        return dp[0][n-1] > 0 ; 
     }
 };
